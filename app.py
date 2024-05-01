5a61d660963fcc64265d5e1ab51d17e6b1499a5d
TomWis97 <github@tomwishaupt.nl>
2024-05-01 22:41:20 +0200

    Implement API calls

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import os
+import re
+import json
+import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -17,4 +21,30 @@ def update():
     regex = r"^#[0-z]{3,6}$"
     if not re.match(regex, new_color):
         abort(400)
-    return "Updating value to: " + new_color
+    with open('/run/secrets/kubernetes.io/serviceaccount/namespace', 'rt') as f:
+        namespace = f.read()
+    with open('/run/secrets/kubernetes.io/serviceaccount/token', 'rt') as f:
+        token = f.read()
+    patch_data = {"data": {"APP_COLOR": new_color}}
+    headers = {
+        "Authorization": "Bearer {}".format(token),
+        "Content-Type": "application/strategic-merge-patch+json",
+        "Accept": "application/json"}
+    reply_patch = requests.patch(
+        "https://{host}:{port}/api/v1/namespaces/{namespace}/configmaps/configmap-test".format(
+            host=os.environ["KUBERNETES_SERVICE_HOST"],
+            port=os.environ["KUBERNETES_SERVICE_PORT_HTTPS"],
+            namespace=namespace),
+        headers=headers,
+        data=json.dumps(patch_data),
+        verify="/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+    rollout_data = {"spec": {"template": {"metadata": {"annotations": {"kubectl.kubernetes.io/restartedAt": datetime.datetime.now().isoformat() }}}}}
+    reply_rollout = requests.patch(
+        "https://{host}:{port}/apis/apps/v1/namespaces/{namespace}/deployments/configmap-test".format(
+            host=os.environ["KUBERNETES_SERVICE_HOST"],
+            port=os.environ["KUBERNETES_SERVICE_PORT_HTTPS"],
+            namespace=namespace),
+        data=json.dumps(rollout_data),
+        headers=headers,
+        verify="/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+    return "Updating value to {}. Patch status code is {}. Rolout status code is {}.".format(new_color, reply_patch.status_code, reply_rollout.status_code)
